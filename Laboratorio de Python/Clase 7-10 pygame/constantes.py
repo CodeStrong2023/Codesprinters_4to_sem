60f2f4c3546923d4a5abbeef9e2c1a08cf6ebfeb
Joaquin Aguero <joaquintobiasagu@gmail.com>
2024-11-09 14:53:14 -0300

    subo clases 7-10 juego pygame

---
+++ Laboratorio de Python/Clase 7-10 pygame/constantes.py
@@ -0,0 +1,15 @@
+#constantes.py
+
+#dimesiones de pantalla
+
+SCREEN_WIDHT = 1700
+SCREEN_HEIGTH = 1000
+
+# ruta de los assets
+ASSETS_PATH = 'C:/Users/eliza/Desktop/Juego_Tuc/assets'
+
+# Rutas de los archivos
+IMPERIAL_MARCH_PATH = f'{ASSETS_PATH}/sounds/Imperial March - kenobi.mp3'
+START_IMAGE_PATH = f'{ASSETS_PATH}/images/inicio/star.png'
+ESTRELLA_PATH = f'{ASSETS_PATH}/images/inicio/estrella'
+FONDO1_PATH = f'{ASSETS_PATH}/images/fondo1.jpg'
---
+++ Laboratorio de Python/Clase 7-10 pygame/explosion.py
@@ -0,0 +1,27 @@
+import pygame
+import os
+
+from constantes import ASSETS_PATH
+
+class Explosion:
+    def __init__(self, x, y):
+        self.images = [pygame.image.load(f'{ASSETS_PATH}/images/regularExplosion0{i:02d}.png') for i in range(9)]
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect(center=(x, y))
+        self.frame_rate = 0
+        self.max_frames = 5
+
+    # Actualizar la pantalla
+    def actualizar(self):
+        self.frame_rate += 1
+        if self.frame_rate >= self.max_frames:
+            self.frame_rate = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                return False
+            self.image = self.images[self.index]
+        return True
+
+    def dibujar(self, screen):
+        screen.blit(self.image, self.rect.topleft)
